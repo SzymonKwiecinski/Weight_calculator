b356d3567a4a812179c157ec44f55fe4519efb3f
SzymonKwiecinski <kwiecinskji@gmail.com>
2022-02-03 22:38:19 +0100

    Update

--- tests/test_position.py
+++ tests/test_position.py
@@ -1,34 +1,33 @@
 from pytest import mark
-from pytest import fixture
 from src.position import Position
 
 
-@fixture('norm, size1, size2, weight, 100szt_kg, kg_100szt',[
+@mark.parametrize('norm, size1, size2, weight, _100szt_kg, kg_100szt',[
     ('DIN931', '10.0', '55', 43.8, 0.228, 4.38)
 ])
-@mark.position
 class PositionTests:
 
-    def test_norm(self, norm):
-        assert 
-
-    def test_size1(position):
+    def test_norm(self, norm, size1, size2, weight, _100szt_kg, kg_100szt):
+        print(norm, size1, size2, weight, _100szt_kg, kg_100szt)
         pass
 
-    def test_size2(position):
-        pass
+    # def test_size1(self):
+    #     pass
 
-    def test_weight(position):
-        pass
+    # def test_size2(self):
+    #     pass
 
-    def test_full_name(position):
-        pass
+    # def test_weight(self):
+    #     pass
 
-    def test_calc_kg_to_100szt(position):
-        pass
+    # def test_full_name(self):
+    #     pass
 
-    def test_calc_100szt_to_kg(position):
-        pass
+    # def test_calc_kg_to_100szt(self):
+    #     pass
 
-    def test_weight_kg_per_1000szt(position):
-        pass
+    # def test_calc_100szt_to_kg(self):
+    #     pass
+
+    # def test_weight_kg_per_1000szt(self):
+    #     pass
